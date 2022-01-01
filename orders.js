f4ff90750a564c34dd7954eed679fcaefd2f079c
Kapil Kumar <kapilprajapati061@gmail.com>
2022-01-02 00:10:31 +0530

    admin-panel-developed

---
+++ orders.js
@@ -0,0 +1,40 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber:'36378',
+        paymentStatus:'Refunded',
+        shipping:'Declined',
+    },
+
+    {
+        productName: 'RUKO F11 Pro Drone',
+        productNumber:'49584',
+        paymentStatus:'Due',
+        shipping:'Pending',
+    },
+    {
+        productName: 'ALONA M2 Camera Drone',
+        productNumber:'85214',
+        paymentStatus:'Paid',
+        shipping:'Delivered',
+    },
+    {
+        productName: 'AJAX S687 Drone',
+        productNumber:'1132',
+        paymentStatus:'Due',
+        shipping:'Declined',
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber:'96584',
+        paymentStatus:'Paid',
+        shipping:'Delivered',
+    },
+    
+]
